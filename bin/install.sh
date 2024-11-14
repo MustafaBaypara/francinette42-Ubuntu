1e5ac10b04de21a68f0c0c7604c3c1c022a27e69
Mustafa Baypara <musti.baypara@gmail.com>
2024-11-14 13:28:09 +0300

    ubuntu

--- bin/install.sh
+++ bin/install.sh
@@ -8,7 +8,7 @@ cd temp_____ || exit
 rm -rf francinette
 
 # download github
-git clone --recursive https://github.com/xicodomingues/francinette.git
+git clone --recursive https://github.com/MustafaBaypara/francinette42-Ubuntu.git
 
 if [ "$(uname)" != "Darwin" ]; then
 	echo "Admin permissions needed to install C compilers, python, and upgrade current packages"
@@ -36,16 +36,6 @@ cd "$HOME" || exit
 rm -rf temp_____
 
 cd "$HOME"/francinette || exit
-
-# start a venv inside francinette
-if ! python3 -m venv venv ; then
-	echo "Please make sure than you can create a python virtual environment"
-	echo 'Contact me if you have no idea how to proceed (fsoares- on slack)'
-	exit 1
-fi
-
-# activate venv
-. venv/bin/activate
 
 # install requirements
 if ! pip3 install -r requirements.txt ; then
